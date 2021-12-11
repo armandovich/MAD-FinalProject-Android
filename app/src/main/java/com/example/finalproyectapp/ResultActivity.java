34eccde14480efc843b420070ed2de68fe0501a2
Simranjit Singh <simranjit.contact@gmail.com>
2021-12-12 00:37:15 +0530

    Result Activity Layout

--- app/src/main/java/com/example/finalproyectapp/ResultActivity.java
+++ app/src/main/java/com/example/finalproyectapp/ResultActivity.java
@@ -3,12 +3,28 @@ package com.example.finalproyectapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    TextView scoreText;
+    ImageButton retryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        scoreText = findViewById(R.id.scoreText);
+        retryButton = findViewById(R.id.retryButton);
+
+
     }
+
+    public void setRetryButton(View View){
+
+    }
+
 }
