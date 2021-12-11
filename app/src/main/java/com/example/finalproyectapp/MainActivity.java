0de4ec1a8fd5615f7d00a958016f529efef768a6
Simranjit Singh <simranjit.contact@gmail.com>
2021-12-12 00:29:36 +0530

    Main Activity Layout

--- app/src/main/java/com/example/finalproyectapp/MainActivity.java
+++ app/src/main/java/com/example/finalproyectapp/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.finalproyectapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start = findViewById(R.id.start);
+    }
+
+    public void setStart(View view) {
+        Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+        startActivity(intent);
     }
 }
