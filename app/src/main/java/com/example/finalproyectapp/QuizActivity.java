11826de6b05d3c27abe175bc8798708042b278c8
Simranjit Singh <simranjit.contact@gmail.com>
2021-12-12 00:35:36 +0530

    Quiz Activity Layout

--- app/src/main/java/com/example/finalproyectapp/QuizActivity.java
+++ app/src/main/java/com/example/finalproyectapp/QuizActivity.java
@@ -2,13 +2,61 @@ package com.example.finalproyectapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class QuizActivity extends AppCompatActivity {
+
+    ImageView questionImage;
+    TextView questionText;
+    RadioGroup radioGroup;
+    ImageButton nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        questionImage = findViewById(R.id.questionImage);
+        questionText = findViewById(R.id.questionText);
+        radioGroup = findViewById(R.id.radioGroup);
+        nextButton = findViewById(R.id.nextButton);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.radioButton1:
+
+
+                        break;
+                    case R.id.radioButton2:
+
+
+                        break;
+                    case R.id.radioButton3:
+
+
+                        break;
+                    case R.id.radioButton4:
+
+
+                        break;
+                }
+            }
+        });
+
+
+    }
+
+    public void setNextButton(View view){
+        //Next Question
+        Intent intent = new Intent(QuizActivity.this, ResultActivity.class);
+        startActivity(intent);
     }
 }
