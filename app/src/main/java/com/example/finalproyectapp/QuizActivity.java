e71001d45f57f5f223ca708fd118497c125ac6e1
Daniel Miolan <miolan96@gmail.com>
2021-12-12 22:02:17 -0400

    Fix layout issues and complete quiz logic

--- app/src/main/java/com/example/finalproyectapp/QuizActivity.java
+++ app/src/main/java/com/example/finalproyectapp/QuizActivity.java
@@ -1,7 +1,12 @@
 package com.example.finalproyectapp;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
@@ -23,15 +28,39 @@ public class QuizActivity extends AppCompatActivity {
 
     ImageView questionImage;
     TextView questionText;
-    RadioGroup radioGroup;
-    RadioButton selectedRadio;
     ImageButton nextBtn;
     ImageButton prevBtn;
     ImageButton submitBtn;
+    RadioGroup rg;
     RadioButton rb1;
     RadioButton rb2;
     RadioButton rb3;
     RadioButton rb4;
+
+    ActivityResultLauncher<Intent> quizAvtivityResult = registerForActivityResult(
+    new ActivityResultContracts.StartActivityForResult(),
+    new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if (result.getResultCode() == Activity.RESULT_OK) {
+                currentQuestion = 0;
+
+                for (int i = 0; i < 5; i++) {
+                    answers.set(i,-1);
+                }
+
+                test.ResetTest();
+                rg.clearCheck();
+                nextBtn.setClickable(true);
+                prevBtn.setClickable(false);
+                submitBtn.setClickable(false);
+                nextBtn.setImageResource(R.drawable.next);
+                prevBtn.setImageResource(R.drawable.prev_hide);
+                submitBtn.setImageResource(R.drawable.submit_hide);
+                UpdateQuestionPanel();
+            }
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +68,11 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
         questionImage = findViewById(R.id.questionImage);
         questionText = findViewById(R.id.questionText);
-        radioGroup = findViewById(R.id.radioGroup);
         nextBtn = findViewById(R.id.nextBtn);
         prevBtn = findViewById(R.id.prevBtn);
         submitBtn = findViewById(R.id.submitBtn);
 
+        rg = findViewById(R.id.radioGroup);
         rb1 = findViewById(R.id.radioButton1);
         rb2 = findViewById(R.id.radioButton2);
         rb3 = findViewById(R.id.radioButton3);
@@ -63,22 +92,39 @@ public class QuizActivity extends AppCompatActivity {
         test = new Test();
 
         UpdateQuestionPanel();
+    }
 
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                switch (checkedId){
-                    case R.id.radioButton1:
-                        break;
-                    case R.id.radioButton2:
-                        break;
-                    case R.id.radioButton3:
-                        break;
-                    case R.id.radioButton4:
-                        break;
-                }
+    public void checkSelection(View view) {
+        switch (view.getId()){
+            case R.id.radioButton1:
+                answers.set(currentQuestion,0);
+                break;
+            case R.id.radioButton2:
+                answers.set(currentQuestion,1);
+                break;
+            case R.id.radioButton3:
+                answers.set(currentQuestion,2);
+                break;
+            case R.id.radioButton4:
+                answers.set(currentQuestion,3);
+                break;
+        }
+
+        boolean canSubmit = true;
+
+        for(int n: answers) {
+            if (n == -1) {
+                canSubmit = false;
             }
-        });
+        }
+
+        if (canSubmit) {
+            submitBtn.setImageResource(R.drawable.submit);
+        } else {
+            submitBtn.setImageResource(R.drawable.submit_hide);
+        }
+
+        submitBtn.setClickable(canSubmit);
     }
 
     public void CheckButtonStatus() {
@@ -94,16 +140,24 @@ public class QuizActivity extends AppCompatActivity {
             prevBtn.setImageResource(R.drawable.prev);
         }
 
-        prevBtn.setClickable(currentQuestion < 4);
+        nextBtn.setClickable(currentQuestion < 4);
         prevBtn.setClickable(currentQuestion > 0);
 
         UpdateQuestionPanel();
 
+        rg.clearCheck();
+
         int tempAnswer = answers.get(currentQuestion);
-        rb1.setChecked(tempAnswer == 0);
-        rb1.setChecked(tempAnswer == 1);
-        rb2.setChecked(tempAnswer == 2);
-        rb3.setChecked(tempAnswer == 3);
+
+        if(tempAnswer == 0) {
+            rb1.setChecked(true);
+        } else if(tempAnswer == 1) {
+            rb2.setChecked(true);
+        } else if(tempAnswer == 2) {
+            rb3.setChecked(true);
+        } else if(tempAnswer == 3) {
+            rb4.setChecked(true);
+        }
     }
 
     public void UpdateQuestionPanel() {
@@ -117,20 +171,26 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     public void nextQuestion(View view) {
-
+        currentQuestion += 1;
+        CheckButtonStatus();
     }
 
     public void prevQuestion(View view) {
-
+        currentQuestion -= 1;
+        CheckButtonStatus();
     }
 
     public void submitTest(View view) {
+        int correctAnswers = 0;
 
-    }
+        for (int i = 0; i < 5; i++) {
+            if (test.CheckAnswer(i,answers.get(i))) {
+                correctAnswers += 1;
+            }
+        }
 
-    public void setNextButton(View view){
-        //Next Question
         Intent intent = new Intent(QuizActivity.this, ResultActivity.class);
-        startActivity(intent);
+        intent.putExtra("correctAnswers", correctAnswers);
+        quizAvtivityResult.launch(intent);
     }
 }
--- app/src/main/java/com/example/finalproyectapp/ResultActivity.java
+++ app/src/main/java/com/example/finalproyectapp/ResultActivity.java
@@ -2,6 +2,7 @@ package com.example.finalproyectapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 public class ResultActivity extends AppCompatActivity {
 
     TextView scoreText;
+    TextView resultText;
     ImageButton retryButton;
 
     @Override
@@ -17,14 +19,32 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        scoreText = findViewById(R.id.scoreText);
+        resultText = findViewById(R.id.testResult);
+        scoreText = findViewById(R.id.testScore);
         retryButton = findViewById(R.id.retryButton);
 
+        int counter = getIntent().getIntExtra("correctAnswers",0);
 
+        String result = "";
+
+        if (counter == 3) {
+            result = "Good job!";
+        } else if (counter == 4) {
+            result= "Excellent work!";
+        } else if (counter == 5) {
+            result = "You are a genius!";
+        } else {
+            result = "Please try again!";
+        }
+
+        resultText.setText(result);
+        scoreText.setText(counter + "/5");
     }
 
-    public void setRetryButton(View View){
-
+    public void retryTest(View View){
+        Intent intent = new Intent();
+        setResult(RESULT_OK, intent);
+        finish();
     }
 
 }
